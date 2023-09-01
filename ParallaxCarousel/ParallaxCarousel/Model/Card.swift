7bdfd24defbece7d83719d14f27289c716296ad2
Gagan Vishal Mishra <gagan.vishal@mmltd.co.za>
2023-09-01 15:39:58 +0200

    Added logic for Parllax effect in CarouselView.

---
+++ ParallaxCarousel/ParallaxCarousel/Model/Card.swift
@@ -0,0 +1,8 @@
+//
+//  Card.swift
+//  ParallaxCarousel
+//
+//  Created by Gagan Vishal  on 2023/09/01.
+//
+
+import Foundation
---
+++ ParallaxCarousel/ParallaxCarousel/View/ParallaxCarouselView.swift
@@ -0,0 +1,18 @@
+//
+//  ParallaxCarouselView.swift
+//  ParallaxCarousel
+//
+//  Created by Gagan Vishal  on 2023/09/01.
+//
+
+import SwiftUI
+
+struct ParallaxCarouselView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ParallaxCarouselView()
+}
